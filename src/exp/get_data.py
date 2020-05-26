5dd60eecf3e1d6ba72388ea31cb4c977dbc78265
P4L <cornelius.weling@gmail.com>
2020-05-26 14:11:46 +0400

    add get-data

--- src/exp/get_data.py
+++ src/exp/get_data.py
@@ -2,25 +2,62 @@ import bq_helper
 from bq_helper import BigQueryHelper
 stackOverflow = bq_helper.BigQueryHelper(active_project="bigquery-public-data",
                                    dataset_name="stackoverflow")
+import pandas as pd
 
-bq_assistant = BigQueryHelper("bigquery-public-data", "stackoverflow")
-# print(bq_assistant.list_tables())
+def get_init_data() -> pd.DataFrame:
+    post_q = """SELECT
+        id AS id_post,
+        owner_user_id AS id_user,
+        score AS post_score,
+        answer_count AS post_ans_count,
+        tags AS post_tags,
+        title AS post_title,
+        body AS post_body,
+        view_count AS post_view_count,
+        creation_date AS post_date,
+        (SELECT reputation FROM `bigquery-public-data.stackoverflow.users` WHERE id = owner_user_id) AS user_rating,
+        (SELECT creation_date FROM `bigquery-public-data.stackoverflow.users` WHERE id = owner_user_id) AS user_reg_date
+    FROM   `bigquery-public-data.stackoverflow.posts_questions`
+    WHERE EXTRACT(YEAR FROM creation_date) = 2019 AND EXTRACT(MONTH FROM creation_date) = 10
+    """
+    resp = stackOverflow.query_to_pandas(post_q)
+    return resp
 
-print(bq_assistant.head("comments", num_rows=20))
+def get_user_quest_count(df: pd.DataFrame) -> pd.DataFrame:
+    return df
 
-query1 = """SELECT
-  EXTRACT(YEAR FROM creation_date) AS Year,
-  COUNT(*) AS Number_of_Questions,
-  ROUND(100 * SUM(IF(answer_count > 0, 1, 0)) / COUNT(*), 1) AS Percent_Questions_with_Answers
-FROM
-  `bigquery-public-data.stackoverflow.posts_questions`
-GROUP BY
-  Year
-HAVING
-  Year > 2008 AND Year < 2016
-ORDER BY
-  Year;
-        """
-response1 = stackOverflow.query_to_pandas_safe(query1)
+def get_user_reached(df: pd.DataFrame) -> pd.DataFrame:
+    return df
 
-print(response1.head(10))
+def get_post_is_closed(df: pd.DataFrame) -> pd.DataFrame:
+    return df
+
+def get_post_type(df: pd.DataFrame) -> pd.DataFrame:
+    return df
+
+def get_all_data(init_data: pd.DataFrame=None) -> pd.DataFrame:
+    df = pd.DataFrame()
+
+    if init_data == None:
+        df = get_init_data()
+    else:
+        df = init_data
+
+    df = get_user_quest_count(df)
+    df = get_post_is_closed(df)
+    df = get_post_type(df)
+    return df
+
+def test():
+    post_q = """SELECT
+    id,
+    reputation
+    FROM `bigquery-public-data.stackoverflow.users`
+    WHERE id = 123
+    """
+    resp = stackOverflow.query_to_pandas_safe(post_q)
+    print(resp.head(10))
+
+if __name__ == "__main__":
+    df = get_all_data()
+    df.to_csv('test.csv', index=False)
