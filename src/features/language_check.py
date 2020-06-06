74bfb9b363dae7bb9423e9e0efe671cd5a7206f9
Kamil <ab.kam@yandex.ru>
2020-06-06 20:51:24 +0300

    added method unnecessary inf

---
+++ src/features/language_check.py
@@ -0,0 +1,5 @@
+import language_check
+tool = language_check.LanguageTool('en-US')
+text = u'A sentence with a error in the Hitchhiker’s Guide tot he Galaxy'
+matches = tool.check(text)
+len(matches)
